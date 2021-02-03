f7ff022216a5e56b8a313180ee7f257c91a9fad0
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2021-02-03 16:13:24 -0500

    Update binarysearch.cpp

--- Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Busquedas/binarysearch.cpp
+++ Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Busquedas/binarysearch.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 #include <cstdlib>
-
-static int binarysearch(int arr[],int l,int r,int x){
-	if(r>=l){
-		int mid=l+(r-1)/2;
+int binarysearch(int arr[],int left,int r,int x){
+	if(r>=left){
+		int mid=left+(r-left)/2;
 			if(arr[mid]==x){
 		return mid;
+        }
+        if(arr[mid]>x){
+            return binarysearch(arr,left,mid-1,x);
+        }else{
+            return binarysearch(arr,mid+1,r,x);
+        }
 	}
-	if(arr[mid]>x){
-		return binarysearch(arr,l,mid-1,x);
-	}else{
-		return binarysearch(arr,mid+1,r,x);
-	}
-	}
-
 	return -1;
 }
 int main() {
-	int arr[]={2,3,4,5,6};
+	int arr[]={1,2,3,5,6};
 	int n=sizeof(arr)/sizeof(arr[0]);
-	int x=5;
+	int x=3;
 	int resultado=binarysearch(arr,0,n-1,x);
 	if(resultado==-1){
 		printf("elemento no presente");
@@ -28,4 +26,3 @@ int main() {
 	}
 	return 0;
 }
-
